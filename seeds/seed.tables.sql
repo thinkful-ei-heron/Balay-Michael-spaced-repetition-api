9b1daa7ac7430d517da73ffe3a6aceedc42809f2
balayaydemir <balayaydemir@gmail.com>
2019-12-29 17:32:33 -0500

    language

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,14 +21,14 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'uygulama', 'practice', 2),
+  (2, 1, 'merhaba', 'hello', 3),
+  (3, 1, 'ev', 'house', 4),
+  (4, 1, 'yazılım mühendisi', 'software developer', 5),
+  (5, 1, 'çevirmek', 'translate', 6),
+  (6, 1, 'inanılmaz', 'amazing', 7),
+  (7, 1, 'köpek', 'dog', 8),
+  (8, 1, 'kedi', 'cat', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
