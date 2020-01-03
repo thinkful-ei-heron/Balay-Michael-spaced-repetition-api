0e13315938a22d37f825a4d759f6c63274f05dbb
Michael Kirsch <michaelskirsch@gmail.com>
2020-01-03 12:25:23 -0700

    CORS

--- src/app.js
+++ src/app.js
@@ -1,25 +1,44 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const errorHandler = require('./middleware/error-handler')
-const authRouter = require('./auth/auth-router')
-const languageRouter = require('./language/language-router')
-const userRouter = require('./user/user-router')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const errorHandler = require('./middleware/error-handler');
+const authRouter = require('./auth/auth-router');
+const languageRouter = require('./language/language-router');
+const userRouter = require('./user/user-router');
 
-const app = express()
+const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}))
-app.use(cors())
-app.use(helmet())
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+  })
+);
 
-app.use('/api/auth', authRouter)
-app.use('/api/language', languageRouter)
-app.use('/api/user', userRouter)
+const corsWhitelist = [
+  'https://spaced-repetition-eight-blue.now.sh',
+  'https://michael-balay-spaced-repetition.now.sh',
+  'https://spaced-repetition.balayaydemir.now.sh'
+];
 
-app.use(errorHandler)
+const corsOptions = {
+  origin: (origin, cb) => {
+    if (!origin || corsWhitelist.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Disallowed by CORS'));
+    }
+  }
+};
 
-module.exports = app
+app.use(cors(corsOptions));
+app.use(helmet());
+
+app.use('/api/auth', authRouter);
+app.use('/api/language', languageRouter);
+app.use('/api/user', userRouter);
+
+app.use(errorHandler);
+
+module.exports = app;
